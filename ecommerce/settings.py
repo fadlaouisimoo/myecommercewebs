415ef8a47a440bad3757f20a702a28c3c66b648a
Mohammed <fadlaouisimo11@gmail.com>
2024-05-20 00:58:29 +0100

    A lot of things has changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'crispy_forms',
 ]
 
 
@@ -131,6 +130,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
